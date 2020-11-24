502623e8a53c22fbcfd0d183065d739d15ba6515
hedi4744 <hedi4744@penguin>
2020-11-24 16:41:27 +0100

    completing sparse

--- Sparse.cpp
+++ Sparse.cpp
@@ -4,7 +4,8 @@ Triplets::Triplets(int num_rows, int num_cols)
 {
     m_num_rows = num_rows ; 
     m_num_cols = num_cols ; 
-    m_entry_data = (float*) malloc(3*sizeof(float)) ; 
+    m_entry_data = (float*) calloc(3*sizeof(float)) ;
+    m_IA = (float*) calloc((m_num_rows+1)*sizeof(float)) ; 
 }
 
 void Triplets::add(int i , int j , float values) 
@@ -24,7 +25,8 @@ void Triplets::ranging()
     int i ; 
     for  (i = 0 ; i<m_triplets_size ; i++) 
     {
-        min = m_entry_data[3*i] ; 
+        min = m_entry_data[3*i] ;
+	index = i ;
         for (int j=i ; j<m_triplets_size ; j++)
         {
             if (m_entry_data[3*j]< min)
@@ -49,7 +51,10 @@ void Triplets::ranging()
 
 void Triplets::settriplet() 
 {
-    ranging() ; 
+    ranging() ;
+    m_values = (float*) calloc(m_triplets_size*sizeof(float)) ;
+    m_IJ = (float*) calloc(m_triplets_size*sizeof(float)) ;
+    m_IA[0] = 0 ; 
     int num_elements_per_row;
     int i,j ;
     int index ;  
@@ -64,7 +69,7 @@ void Triplets::settriplet()
             j++ ;  
         }
         index = (int) m_entry_data[3*i] ;
-        i = j+1 ; 
+        i = j ; 
         m_IA[index+1] = num_elements_per_row ; 
     }  
     if (m_entry_data[3*(m_triplets_size-2)] == m_entry_data[3*(m_triplets_size-1)]) 
@@ -80,11 +85,51 @@ void Triplets::settriplet()
     for (int i=0 ; i<m_triplets_size ; i++) 
     {
         m_values[i] = m_entry_data[3*i+2] ; 
-        m_IJ[i] = m_entry_data[3*i+1] ; 
+        m_IJ[i] = (int) m_entry_data[3*i+1] ; 
 
     }
 }
 
+SparseMAtrix::SparseMatrix(int num_rows , int num_cols)
+{
+  m_num_rows = num_rows ;
+  m_num_cols = num_cols ;
+  Triplet = Triplets(num_rows , num_cols) ; 
+}
+
+void SparseMatrix::add_value(int i , int j , float value)
+{
+  Triplet.add(i,j,value) ; 
+}
+
+void SparseMatrix::set_matrix()
+{
+  Triplet.settriplet() ;
+  m_IA = Triplet.get_m_IA() ;
+  m_IJ = Triplet.get_m_IJ() ;
+  m_values = Triplet.get_m_values() ; 
+}
+
+float* SparseMatrix::vector_mult(float* x)
+{
+  float* result ;
+  result = (float*) calloc(n_num_rows*sizeof(float)) ;
+
+  int num_element_per_row ; 
+  int index = 0 ; 
+  for (int i=0 ; i<m_num_rows ; i++)
+    {
+      num_element_per_row = m_IA[i+1] - m_IA[i] ;
+      result[i] = 0 ; 
+      for (int j=0 ; j<num_element_per_row ; j++)
+	{
+	  index = index + j ;
+	  result[i] += m_values[index]*x[m_IJ[index]] ;
+	}
+      index++ ; 
+    }
+  
+}
 
 
 
--- Sparse.h
+++ Sparse.h
@@ -10,37 +10,60 @@ class Triplets
         int m_num_cols ,m_num_rows ; 
         int m_triplets_size ; 
         float* m_entry_data ; 
-        float* m_IA ; 
-        float* m_IJ ; 
+        int* m_IA ; 
+        int* m_IJ ; 
         float* m_values ;
         void ranging() ; 
     public: 
         Triplets(int m_num_rows, int m_num_cols) ;  
-        void add(int i , int j , float value) ;
-        // sequential 
+        void add(int i , int j , float value) ; 
         void settriplet() ;
-        //MPI  
-        //void MPI_settriplet() ;
-        //cuda  
-        //void cuda_settriplet() ;
-
+        int* get_m_IA() {return m_IA ; } ;
+        int* get_m_IJ() {return m_IJ ; } ;
+        float* get_m_values(){return m_values ; } ; 
 };
 
-/*
+
 class SparseMatrix
 {
     private: 
         int m_number_rows  , m_number_columns ; 
+        Triplets Triplet ;  
         float* m_values ; 
         float* m_IA ; 
         float* m_IJ ; 
     public: 
         SparseMatrix(int number_rows , int number_columns) ; 
         void add_value(int i , int j , float value) ; 
-        void set_matrix() ;
-        float* cuda_vector_mult(float* vect) ; 
-        float* vector_mult(float* vect)
+        void set_matrix() ; 
+        float* vector_mult(float* vect) ;
 
-}; 
+};
 
-*/
+class Solver
+{
+protected : 
+  SparseMatrix* m_A ;
+  float* m_X ;
+  float* m_b ; 
+public :
+  Solver(SparseMatrix* A , float* m_b ) ;
+  virtual void solve() ;
+  virtual float* get_solution() ; 
+};
+
+class CG_solver : public Solver 
+{
+public :
+  CG_solver(SparseMatrix* A, float* m_b) ;
+  virtual void solve() ;
+};
+
+class BICGSTAB_solver : public Solver
+{
+public:
+  BICGSTAB_solver() ;
+  virtual void solve() ; 
+};
+
+
